e74b4f881332a8a3461191dbbf64a136f96a604c
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-10-23 00:43:22 +0530

    Update main.tf

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -2,8 +2,7 @@ provider  "aws" {
   region     =   "us-east-1"
 }
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
-
+  bucket = var.bucket_name
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
