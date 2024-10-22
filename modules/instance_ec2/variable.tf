8177c5895aa66c6983e4e3b10e1644d7a66eb355
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-10-23 00:36:32 +0530

    Create variable.tf

---
+++ modules/instance_ec2/variable.tf
@@ -0,0 +1,12 @@
+variable  "ami"  {
+   description  = "The ami id we have to give"
+   type         = string
+}
+variable  "instance_type" {
+   description  = "Instance type we are giving"
+  type          = string
+}
+variable  "subnet_id" {
+   description   = "subnet id we have to assign"
+   type          = string
+} 
