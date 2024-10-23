4c5dc8b0079c1aa9d7ca2bd28d5fff2a4b80d30b
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-10-23 20:29:48 +0530

    Delete modules/vpc directory

--- modules/vpc/main.tf
+++
@@ -1,9 +0,0 @@
-module "vpc" {
-  source              = "./modules/vpc"
-  region              = "us-east-1"
-  cidr_block          = "10.0.0.0/16"
-  vpc_name            = "main"
-  public_subnet_cidr  = "10.0.1.0/24"
-  private_subnet_cidr = "10.0.2.0/24"
-  availability_zone   = "us-east-1a"
-}
