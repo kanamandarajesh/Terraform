6fac641b475d05533cd1035ebca7fe0d7bdfc9df
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-10-24 20:33:46 +0530

    Update main.tf

--- configuration-files/s3-bucket/main.tf
+++ configuration-files/s3-bucket/main.tf
@@ -2,7 +2,7 @@ provider  "aws"  {
   region      =  "us-east-1"
 }  
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
+  bucket = "my-tf-test-123-bucket-database"
 
   tags = {
     Name        = "My bucket"
