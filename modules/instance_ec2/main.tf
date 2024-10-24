be84a196d54a624c2d9c7318b5128ccef4ae38c9
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-10-24 20:39:57 +0530

    Update main.tf

--- modules/instance_ec2/main.tf
+++ modules/instance_ec2/main.tf
@@ -5,5 +5,5 @@ provider   "aws"  {
 resource "aws_instance" "example" {
     ami           = var.ami
     instance_type = var.instance_type
-    subnet_id     = var.subnet_type
+    subnet_id     = var.subnet_id
 }
